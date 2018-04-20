8954ab54c21bc0d92c8136653d51f1230c9be72b
oshipston <shipston.sharman@gmail.com>
2018-04-20 13:54:42 +0100

    Added coverURL Scape to book_scrape
    Added a function to scrape the book covers from the mian BMJ articles.
    
    Also tidied up book_scrape

--- lib/php/book_scrape.php
+++ lib/php/book_scrape.php
@@ -26,6 +26,18 @@
         return $priceStr;
     }
 
+    // Function to scrape book cover URLs from main article...
+    function coverURL($postURL){
+      $html = file_get_html($postURL);
+      $coverImg = $html->find('.entry-content img')[0];
+      if (is_null($coverImg)){
+        $coverURL = '';
+      } else {
+        $coverURL = $coverImg->getAttribute('src');
+      }
+      return $coverURL;
+    }
+
     // Create mySQL connection
     $con = new mysqli($servername, $username, $password, $dbname);
     mysqli_set_charset($con,"utf8");
@@ -38,34 +50,39 @@
 
     // Download BMJ Blog list by each page
     $pg=1;
-    while ($pg < 4) {
+    while ($pg < 2) {
       echo 'Page = '.$pg.'<br>'; //Debugging Line
       $html = file_get_html('http://blogs.bmj.com/medical-humanities/category/book-reviews/page/'.$pg.'/');
       // For each post..
-      //$po = 0; //DB count
-      foreach ($html->find('article.post') as $post){
-        //Pull post information..
-        $postDat['Header'] = $post->find('header.entry-header')[0]; //Pull header of post
-        $postDat['Title'] = $postDat['Header']->find('.entry-title')[0]->getAttribute('plaintext'); //Discern whether a book review...
-        $postDat['Date'] = $postDat['Header']->find('.posted-on')[0]->getAttribute('plaintext');
-        $postDat['Author'] = $postDat['Header']->find('.author')[0]->getAttribute('plaintext');
-        $postDat['AuthorURL'] = $postDat['Header']->find('span.author a')[0]->getAttribute('href');
-        $postDat['Blurb'] = $post->find('.entry-content p')[0]->getAttribute('plaintext');
-        $postDat['BookPrice'] = bookPrice($postDat['Blurb']); //Try and find book price in blurb..
-        echo $postDat['BookPrice'].'<br>';
-        $postDat['URL'] = $post->find('.entry-content .shareaholic-canvas')[0]->getAttribute('data-link');
-        // Pull book title..
-        // 'Book review: ' is a common prefix so check for it...
-        if (strpos($postDat['Title'],'Book Review:') !== false) {
-          // If present slice string...
-          $postDat['BookTitle'] = substr($postDat['Title'],strlen('Book Review:'),(strlen($postDat['Title'])-strlen('Book Review:')));
-        } else {
-          // If not ASSUME book title is the same as the article...
-          $postDat['BookTitle'] = $postDat['Title'];
+      if (is_null($html)) {
+        echo 'No html on page';
+        break;
+      } else {
+        foreach ($html->find('article.post') as $post){
+          //Pull post information..
+          $postDat['Header'] = $post->find('header.entry-header')[0]; //Pull header of post
+          $postDat['Title'] = $postDat['Header']->find('.entry-title')[0]->getAttribute('plaintext'); //Discern whether a book review...
+          $postDat['Date'] = $postDat['Header']->find('.posted-on')[0]->getAttribute('plaintext');
+          $postDat['Author'] = $postDat['Header']->find('.author')[0]->getAttribute('plaintext');
+          $postDat['AuthorURL'] = $postDat['Header']->find('span.author a')[0]->getAttribute('href');
+          $blurbStr = $post->find('.entry-content p')[0]->getAttribute('plaintext'); // Pull blurb text
+          $postDat['Blurb'] = substr($blurbStr,0,(strlen($blurbStr)-10)); // Cut end off blurb text
+          $postDat['URL'] = $post->find('.entry-content .shareaholic-canvas')[0]->getAttribute('data-link');
+          $postDat['BookPrice'] = bookPrice($postDat['Blurb']); //Try and find book price in blurb..
+          $postDat['CoverURL'] = coverURL($postDat['URL']); //Try and cover source in post HTML...
+          // Pull book title..
+          // 'Book review: ' is a common prefix so check for it...
+          if (strpos($postDat['Title'],'Book Review:') !== false) {
+            // If present slice string...
+            $postDat['BookTitle'] = substr($postDat['Title'],strlen('Book Review:'),(strlen($postDat['Title'])-strlen('Book Review:')));
+          } else {
+            // If not ASSUME book title is the same as the article...
+            $postDat['BookTitle'] = $postDat['Title'];
+          }
+          echo $postDat['BookTitle'].'<br>';
         }
-        //$po++; //DB Count
+        $pg++;
       }
-      $pg++;
     }
     $con->close();
 ?>
